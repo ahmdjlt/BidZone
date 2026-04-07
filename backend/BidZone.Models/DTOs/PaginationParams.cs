bfc5a3af1db756974d65c87105a93856e4ff7500
ssh-roman <roman.ghidenco@isa.utm.md>
2026-04-07 15:50:00 +0300

    backend: add PaginatedResult and PaginationParams models

---
+++ backend/BidZone.Models/DTOs/PaginationParams.cs
@@ -0,0 +1,15 @@
+namespace BidZone.Models.DTOs;
+
+public class PaginationParams
+{
+    private const int MaxPageSize = 50;
+    private int _pageSize = 12;
+
+    public int Page { get; set; } = 1;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+    }
+}
