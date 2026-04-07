794acbfc23b0e83e2374fe91f9a4c8b91c029d29
ssh-roman <roman.ghidenco@isa.utm.md>
2026-04-07 17:05:00 +0300

    backend: add FluentValidation with CreateAuctionValidator

---
+++ backend/BidZone.BLL/Validators/CreateAuctionValidator.cs
@@ -0,0 +1,34 @@
+using BidZone.Models.DTOs;
+using FluentValidation;
+
+namespace BidZone.BLL.Validators;
+
+public class CreateAuctionValidator : AbstractValidator<CreateAuctionDto>
+{
+    public CreateAuctionValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title is required")
+            .MaximumLength(200).WithMessage("Title must not exceed 200 characters");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required");
+
+        RuleFor(x => x.ImageUrl)
+            .MaximumLength(500).When(x => x.ImageUrl != null);
+
+        RuleFor(x => x.StartingPrice)
+            .GreaterThan(0).WithMessage("Starting price must be greater than 0");
+
+        RuleFor(x => x.ReservePrice)
+            .GreaterThanOrEqualTo(x => x.StartingPrice)
+            .When(x => x.ReservePrice.HasValue)
+            .WithMessage("Reserve price must be at least the starting price");
+
+        RuleFor(x => x.EndTime)
+            .GreaterThan(DateTime.UtcNow).WithMessage("End time must be in the future");
+
+        RuleFor(x => x.CategoryId)
+            .GreaterThan(0).WithMessage("Category is required");
+    }
+}
