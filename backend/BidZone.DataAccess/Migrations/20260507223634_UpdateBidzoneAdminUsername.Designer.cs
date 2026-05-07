8306fbf0c112d6733e5bf809b17b23c8fda649e8
Ahmed Jalilati <127544477+ahmdjlt@users.noreply.github.com>
2026-05-08 01:40:13 +0300

    Add migration for bidzone admin username rename

--- backend/BidZone.DataAccess/Migrations/20260507223634_UpdateBidzoneAdminUsername.Designer.cs
+++ backend/BidZone.DataAccess/Migrations/20260507223634_UpdateBidzoneAdminUsername.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BidZone.DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BidZone.DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260507223634_UpdateBidzoneAdminUsername")]
+    partial class UpdateBidzoneAdminUsername
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/BidZone.DataAccess/Migrations/20260507223634_UpdateBidzoneAdminUsername.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BidZone.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBidzoneAdminUsername : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1000,
+                columns: new[] { "NormalizedUserName", "Username" },
+                values: new object[] { "BIDZONE", "bidzone" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1000,
+                columns: new[] { "NormalizedUserName", "Username" },
+                values: new object[] { "BIDZONE_ADMIN", "bidzone_admin" });
+        }
+    }
+}
