4e67b77fd9b083ae39fc53c74ce889b6885b78ac
Ahmed Jalilati <127544477+ahmdjlt@users.noreply.github.com>
2026-06-10 16:34:11 +0300

    Add migration: update auction seed seller

---
+++ backend/BidZone.DataAccess/Migrations/20260609225905_UpdateAuctionSeedSeller.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BidZone.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAuctionSeedSeller : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Auctions",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "SellerId",
+                value: 1000);
+
+            migrationBuilder.UpdateData(
+                table: "Auctions",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "SellerId",
+                value: 1000);
+
+            migrationBuilder.UpdateData(
+                table: "Auctions",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "SellerId",
+                value: 1000);
+
+            migrationBuilder.UpdateData(
+                table: "Auctions",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "SellerId",
+                value: 1000);
+
+            migrationBuilder.UpdateData(
+                table: "Auctions",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "SellerId",
+                value: 1000);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Auctions",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "SellerId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Auctions",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "SellerId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Auctions",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "SellerId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Auctions",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "SellerId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Auctions",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "SellerId",
+                value: 2);
+        }
+    }
+}
